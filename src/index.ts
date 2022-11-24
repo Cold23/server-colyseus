c510583f329abcf83846e9cb48fafba958a45d42
Cold23 <envans.doni@gmail.com>
2022-11-24 21:01:48 +0200

    Remove cors

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ import { MyRoom } from './rooms/MyRoom'
 const port = Number(process.env.PORT || 2567)
 const app = express()
 
-app.use(cors())
 app.use(express.json())
 
 const server = http.createServer(app)
